f6478a498a545901c40db97d56c38667ed4b8342
Rishi Sharma <110768656+ssrishi@users.noreply.github.com>
2023-04-26 20:35:30 +0530

    Add files via upload

---
+++ JavaScript/JS Practice/Introduction.js
@@ -0,0 +1,42 @@
+// DataTypes in JavaScript
+
+// 1) Number
+// All the number whether they are decimal or integer are come under this DataType. Example 7 is a Number DataType or 2.5 is also Number DataType.
+
+// 2) String
+//  All the data you type in single Quotes or Double Quotes is considered as Sting.
+
+// 3) Boolean
+// We get only 2 Values Which are:
+// True means 1
+// False means 0
+
+// 4) Empty 
+// Empty contains thwo value 1)Null 2)Undefined
+
+// All 4 are primitive datatype
+
+// Now non-primitive DataType
+
+// 1) Array
+// In array we can store multiple DataType Values
+// Example [1,2,"Array",True] this array contains Values of 3 DataTypes
+// Array works on index(position) means 1st Value in the array has 0 index(position) After 2nd has index of 1 and so on.
+
+// 2) Objects
+// In t Objects Key and value pair works
+// Example {
+// Key:Value
+// }
+
+// Example {
+// fname:Rishi   fname is the key and Rishi is the value.
+// }
+
+let x=7  ,y='7';
+if(x===y){
+    console.log("Correct");
+}
+else{
+    console.log("Wrong")
+}
---
+++ JavaScript/JS Practice/JS Methods and Loops.js
@@ -0,0 +1,61 @@
+// const now = new Date(); 
+// console.log(now);
+// console.log(now.toString());
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDay());
+// console.log(now.getTime());
+// console.log(now.getDate());
+
+// console.log(now.getHours());
+// console.log(now.getMinutes());
+// console.log(now.getSeconds());
+
+// MAth
+
+// const pi = Math.PI;
+// console.log(pi);
+
+// console.log(Math.round(pi));
+// console.log(Math.ceil(pi));
+// console.log(Math.floor(pi));
+
+// console.log(Math.min(5,10,15,21,1));
+// console.log(Math.max(5,10,15,21,1));
+
+// Random
+
+// console.log(Math.round(Math.random(0,1)*100));
+
+// console.log(Math.pow(4,3));
+
+// console.log(Math.log(2));
+
+// console.log(Math.sin(60));
+
+
+// loops (5 loops)
+
+// do while loop
+// let i =6;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<5)
+
+
+// while loop
+// let i=6
+// while(i<7){
+//     console.log(i);
+//     i++;
+// }
+
+
+// for loop
+
+// let i;
+// for(i=1;i<5;i++)
+// {
+//     console.log(i);
+// }
