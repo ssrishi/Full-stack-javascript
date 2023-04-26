89cd2749603e0598a5e8f3157aea0bcfc7ebebf3
Rishi Sharma <110768656+ssrishi@users.noreply.github.com>
2023-04-26 20:39:19 +0530

    Add files via upload

---
+++ JavaScript/JS Practice/Functions.js
@@ -0,0 +1,47 @@
+// Functions With out parameters or arguments
+// function sum(){                     // this is called function define 
+//     let var1=10,var2=20;
+//     console.log(var1+var2);
+// }
+// sum()   // --> this is how we call function this is called function calling
+// we call functions for n number of times but we need to only define one time.
+
+// Functions with parameter
+// function add(var1, var2){  
+//     console.log(var1+var2);
+// }
+// here we dont need to declare  var1 and var2 because while defining function var1 and var2 are already declared.
+
+// add(11,1342);
+// add(111,12);
+// add(232,12);
+// add(11,92);
+// add(67,12);
+// add(31,12);
+
+// functions with retutn type
+// Actually functions are define in the way that the do some specific task and return result.
+
+// function modulous(n1,n2){
+//     return (n1%n2);
+// }
+// let result = modulous(5,2);
+// console.log(result);
+
+// function sum(arr) {
+//         let sum = 0,n=0;
+//         for(n of arr){
+//             sum = sum + n;
+//         }
+//         return sum;
+// }
+//     let sumarray = [1, 2, 3, 4, 5, 6, 7, 9, 8];
+//     let arrresult = sum(sumarray);
+//     console.log(arrresult);
+
+// let username = {
+//     name : 'rishi',
+//     m_no: 8373995918,
+//     pass: '1267'
+// }
+// console.log(username.name);
