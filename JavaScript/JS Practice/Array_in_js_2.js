efa7648080b2035dd3a5eb68b5623f254b557532
Rishi Sharma <110768656+ssrishi@users.noreply.github.com>
2023-05-11 12:34:04 +0530

    Create Array_in_js_2.js

---
+++ JavaScript/JS Practice/Array_in_js_2.js
@@ -0,0 +1,16 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)   push is not a write method to merge to array because it will not merge it will add the whole array as an element of the marvel_
+//                               heros array print and check. 
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)  concat is the write method to merge ore than one array it will merge and return an array.    
+// console.log(allHeros);
+
+
+// const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+// const real_another_array = another_array.flat(Infinity)  //Flat methhod is use to spread an array. If there are more than one array in an array and you want to ake one array you can use flat. 
+// console.log(real_another_array);
