93b0ceac48776e82cc99471bccb1c7a398a55f70
Rishi Sharma <110768656+ssrishi@users.noreply.github.com>
2023-05-15 13:10:16 +0530

    Create index.js

---
+++ JavaScript/JS Practice/Onjects_in_JS/index.js
@@ -0,0 +1,10 @@
+// Every Object in a JavaScript is an instance of object.
+const object = {
+    fname: 'rishi',
+    lname: 'sharma'
+}
+
+// for (k in object){
+//     console.log(object[k]);
+// }
+// String, number, BigInt, boolean, null, undefined, Symbol these are not onjects
