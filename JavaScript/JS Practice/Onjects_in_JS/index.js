879600d36ec8385ba7faa2616e66e7016776798c
Rishi Sharma <110768656+ssrishi@users.noreply.github.com>
2023-05-15 13:11:21 +0530

    Update index.js

--- JavaScript/JS Practice/Onjects_in_JS/index.js
+++ JavaScript/JS Practice/Onjects_in_JS/index.js
@@ -8,3 +8,22 @@ const object = {
 //     console.log(object[k]);
 // }
 // String, number, BigInt, boolean, null, undefined, Symbol these are not onjects
+
+
+// 1st way to declare object
+// const obj1 ={
+//     car: 'Tata',
+//     Mileage: '30Km/H',
+//     greeting:{
+//         msg:function() {
+//             console.log('Go and Have a ride in your Wonderful Car')
+//         }
+//     }
+// }
+// obj1.notice = {
+//     msg1:function() {
+//         console.log("You will get 3 services free. Dont forget of service and Maintain your Car")
+//     }
+// }
+
+// console.log(obj1);
