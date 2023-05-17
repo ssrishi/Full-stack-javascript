d05b9aa3c3096426ca1021dd3be2d73e17ef543e
Rishi Sharma <110768656+ssrishi@users.noreply.github.com>
2023-05-17 13:01:48 +0530

    Create objects_02.js

---
+++ JavaScript/JS Practice/objects_02.js
@@ -0,0 +1,15 @@
+// More on objects
+
+// Object.assign() Method => Copies the values of all enumerable own properties from one or more source objects to a target object.
+
+// const target = {
+//     a:1,
+//     b:2
+// }
+// const source = {
+//     a:4,
+//     d:5
+// }
+
+// const output_obj = Object.assign(target,source) 
+// console.log(output_obj);
