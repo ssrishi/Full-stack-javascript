5e393d482e7eaf9f865e4a1f310a5ce6cbdb5a8d
Rishi Sharma <110768656+ssrishi@users.noreply.github.com>
2023-07-11 11:14:12 +0530

    Update variable.js

--- JS ASSIGNMENT/Q-1/variable.js
+++ JS ASSIGNMENT/Q-1/variable.js
@@ -2,3 +2,11 @@
 let myname = "Rishi";
 
 console.log(`Value of variable ${myname} And Type of variable ${typeof(myname)}`);
+
+// Boolean
+
+let X = true;
+let Y = false;
+
+console.log(`Value of variable ${X} And Type of variable ${typeof(X)}`);
+console.log(`Value of variable ${Y} And Type of variable ${typeof(Y)}`);
