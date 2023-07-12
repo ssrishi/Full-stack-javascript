d1c9d174b3aeb18d3708eea69eb519dc84882d60
Rishi Sharma <110768656+ssrishi@users.noreply.github.com>
2023-07-12 11:31:52 +0530

    Update q2.js

--- JS ASSIGNMENT/Q-2/q2.js
+++ JS ASSIGNMENT/Q-2/q2.js
@@ -3,3 +3,5 @@ const last_name = "Sharma";
 let marital_status = "Unmarried";
 const country = "India";
 let age = 23;
+
+console.log(`My name is ${first_name} ${last_name}. I am from ${country}. I'm ${age} years old and ${marital_status}`);
