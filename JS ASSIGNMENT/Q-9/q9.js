f0851c0607a52b057dee83ec632f92c0dc7429e1
Rishi Sharma <110768656+ssrishi@users.noreply.github.com>
2023-07-14 14:35:45 +0530

    Add files via upload

---
+++ JS ASSIGNMENT/Q-9/q9.js
@@ -0,0 +1,14 @@
+// Write three JavaScript statement example which provide truthy value.
+let number = 10;
+
+let str = "Hello, World!";
+
+let array = [1, 2, 3];
+
+
+// Write three JavaScript statement example which provide falsy value.
+let bool = false;
+
+let emptyString = "";
+
+let nullValue = null;
