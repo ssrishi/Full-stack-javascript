80c177bf42e877e1e05c359359a4f0a6dba7e00f
Rishi Sharma <110768656+ssrishi@users.noreply.github.com>
2023-07-14 14:33:27 +0530

    Add files via upload

---
+++ JS ASSIGNMENT/Q-4/q4.js
@@ -0,0 +1,10 @@
+let variable = "The script of this is great"
+
+let answer = variable.includes("script");
+
+if (answer == true){
+    console.log("This string has word script");
+}
+else{
+    console.log("This string does not have word script");
+}
