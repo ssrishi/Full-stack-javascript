6b38c755d5de54a2cc1102b40154aef589aed3ea
Rishi Sharma <110768656+ssrishi@users.noreply.github.com>
2023-07-14 14:34:42 +0530

    Add files via upload

---
+++ JS ASSIGNMENT/Q-7/q7.js
@@ -0,0 +1,10 @@
+const institutes = [
+    "ineuron",
+    "PwSkills",
+    "CodingNinjas",
+    "PwSkills",
+    "LearnCodeOnline",
+  ];
+  
+  console.log(`The First Occurence of PwSkills is at ${institutes.lastIndexOf("PwSkills",2)}st index`);
+  console.log(`The last Occurence of PwSkills is at ${institutes.lastIndexOf("PwSkills")}th index`);
