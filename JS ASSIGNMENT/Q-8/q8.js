b7f142b4390484150fd1bca1b2d0cbd2a0c62e2f
Rishi Sharma <110768656+ssrishi@users.noreply.github.com>
2023-07-14 14:35:00 +0530

    Add files via upload

---
+++ JS ASSIGNMENT/Q-8/q8.js
@@ -0,0 +1,7 @@
+let string = "  Myname  "
+
+console.log(`length of string including white spaces is ${string.length}`);
+
+console.log(`\nUse trim function to get actual length without whitespaces`);
+
+console.log(`\nActual length is ${string.trim().length}`);
