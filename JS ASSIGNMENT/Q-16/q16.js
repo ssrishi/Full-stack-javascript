be323944af5888a3932124e1f44cd145b2599651
Rishi Sharma <110768656+ssrishi@users.noreply.github.com>
2023-07-15 14:54:58 +0530

    Add files via upload

---
+++ JS ASSIGNMENT/Q-16/q16.js
@@ -0,0 +1,14 @@
+let month = "December";
+
+if(month == "September" || month == "October" || month == "November"){
+    console.log("The season is Autum");
+}
+else if(month == "December" || month == "January" || month == "February"){
+    console.log("The season is Winter");
+}
+else if(month == "March" || month == "April" || month == "May"){
+    console.log("The season is Spring");
+}
+else{
+    console.log("This season is Summer");
+}
