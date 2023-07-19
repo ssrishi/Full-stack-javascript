11c7017b8d73da50ed59fae38e3b429107d02692
Rishi Sharma <110768656+ssrishi@users.noreply.github.com>
2023-07-19 13:38:19 +0530

    Update prototype.js

--- JavaScript/JS Practice/prototype.js
+++ JavaScript/JS Practice/prototype.js
@@ -1,2 +1,11 @@
 let myHeros = ["thor", "spiderman"]
 let dcHeros = ["batman", "black adam", "superman"]
+
+let heropower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
